1a51a06b342d328ce063b1618ba4445c2b4790cb
vkpoladi <80285249+vkpoladi@users.noreply.github.com>
2021-09-07 15:19:18 -0600

    Add files via upload

---
+++ caesar.py
@@ -0,0 +1,12 @@
+//First assignment
+def encrypt(key,plaintext):
+    ciphertext=""
+    #YOUR CODE HERE
+    return ciphertext
+
+def decrypt(key,ciphertext):
+    plaintext=""
+    #YOUR CODE HERE
+    return plaintext
+
+
